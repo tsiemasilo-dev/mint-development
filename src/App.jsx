86f750b778dc16f07cdadbe280f456e6015ffaa7
mihle-matimba <141812759+mihle-matimba@users.noreply.github.com>
2026-01-16 14:46:13 +0200

    Remove preloader components

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,5 @@
-import { useEffect, useState } from 'react';
-
-import Preloader from './components/ui/preloader.tsx';
 import AuthPage from './pages/AuthPage.jsx';
 
-const App = () => {
-  const [showPreloader, setShowPreloader] = useState(true);
-
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setShowPreloader(false);
-    }, 1200);
-
-    return () => clearTimeout(timeoutId);
-  }, []);
-
-  if (showPreloader) {
-    return <Preloader message="Loading Mint" />;
-  }
-
-  return <AuthPage />;
-};
+const App = () => <AuthPage />;
 
 export default App;
