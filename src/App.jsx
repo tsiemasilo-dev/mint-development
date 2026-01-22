46d0359c1edc74a697e2c0e3f254cfc1ac14ef47
mihle-matimba <141812759+mihle-matimba@users.noreply.github.com>
2026-01-22 11:53:33 +0200

    Change initial currentPage state to 'auth'

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
   const [showPreloader, setShowPreloader] = useState(true);
   
   // Set to 'main' so we skip onboarding and see the Navbar immediately
-  const [currentPage, setCurrentPage] = useState("main"); 
+  const [currentPage, setCurrentPage] = useState("auth"); 
   const [activeTab, setActiveTab] = useState("home");
   const [authStep, setAuthStep] = useState("email");
 
